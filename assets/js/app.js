27a3e9f16a3242e43d943cbfccbb768fe69acb2c
donsingh <dbmsph@gmail.com>
2017-01-20 12:50:51 +0800

    what

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,12 @@ var ctr = 0;
 var c = 1;
 $(document).ready(function(){
 
-    insert(ctr);
-    insert(ctr);
-    insert(ctr);
-    insert(ctr);
-    insert(ctr);
-
     $(".btn-primary").on("click", function(){
-        move_trav(c++);
+        $(".media").show();
+    });
+
+    $(".btn-success").on("click", function(){
+        insertNode(ctr++);
     });
 
     $(".stage").on("mouseenter", "div", function(){
@@ -30,18 +28,68 @@ $(document).ready(function(){
     });
 });
 
+function insertNode(step)
+{
+    switch(step){
+        case 0:
+            $(".subtitle").text("We are now going to insert a new node in the list!");
+            $(".code-text").html("<span class='strong'>void insertNode(struct node **head)</span><br>{<br>");
+            break;
+
+        case 1:
+            var obj = "<div class='wrapper' id='trav'><div class='ptr'></div><p>TRAV</p></div>";
+            $(".subtitle").html("First We Create Variable <code>trav</code>");
+            $(".code-text").children().removeClass("strong");
+            $(".code-text").append("&nbsp;&nbsp;&nbsp;<span class='strong'>struct node *trav;</span>");
+            $(".stage").prepend(obj);
+            break;
+
+        case 2:
+            $("#trav").prepend("<div class='arrow2'></div>");
+            $(".subtitle").html("<code>trav</code> will be given address of <code>head</code>");
+            $(".code-text").children().removeClass("strong");
+            $(".code-text").append("<br>&nbsp;&nbsp;&nbsp;<span class='strong'>trav = &head;</span>");
+            $("#trav > .ptr").text("&head");
+            break;
+
+        case 3:
+            $(".subtitle").html("Is <code>trav</code> pointing to an empty pointer?<br><small>Empty pointers signify the end of the list</small>");
+            $(".code-text").children().removeClass("strong");
+            $(".code-text").append("<br>&nbsp;&nbsp;&nbsp;<span class='strong'>while(*trav != NULL){}</span>");
+            $("#trav > .ptr").text("&head");
+            break;
+
+        case 4:
+            $(".subtitle").html("Now that we know <code>trav</code> is at the end of the list...");
+            $(".code-text").children().removeClass("strong");
+            break;
+
+        case 5:
+            $(".subtitle").html("Let's create a new node-type structure!");
+            $(".code-text").append("<br>&nbsp;&nbsp;&nbsp;<span class='strong'>struct node *link = (struct node*) malloc(sizeof(struct node));</span>");
+            insert(1);
+            break;
+
+        case 6:
+            $(".subtitle").html("Let's assign that newly created structure to the last part of the list!");
+            $("#head").children(".list").text("node1");
+            connect();
+            break;
+    }
+
+}
 
 function insert(step)
 {
-    var data;
-    if(step === 0){
-        $("#head").append("<div class='arrow'></div>");
-    }else{
-        data = 15;
-        create_node(ctr,data);
-    }
-    ctr++;
+    var obj = "<div class='node' id='node"+step+"'><p class='float:left; text-align:center'>0x0A1</p><div class='data'></div><div class='ptr next'></div></div>";
+    $(".stage").append(obj);
 }
+
+function connect()
+{
+    $(".list").parent().append("<div class='arrow'></div>");
+}
+
 
 function move_trav(step)
 {
